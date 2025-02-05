ce6433ee88022556068574394983a904b05ca05f
ayush <ayush0188.becse24@chitkara.edu.in>
2025-02-05 22:16:59 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    chat a;
+    scanf("%c",&a);
+    switch(a){
+        case 'A':
+        printf("Excellent");
+        break;
+        case 'B':
+        printf("Good");
+        break;
+        case 'C':
+        printf("Average");
+        break;
+        case 'D':
+        printf("Below Average");
+        break;
+        case 'E':
+        printf("Fail");
+        break;
+        default:
+        printf("Invalid Grade");
+    }
+}
