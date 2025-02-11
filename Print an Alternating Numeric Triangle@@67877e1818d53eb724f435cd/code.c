5c668ac252ceac810b11879c3b6a430279c78bdd
ayush <ayush0188.becse24@chitkara.edu.in>
2025-02-11 15:43:07 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -1,60 +1,17 @@
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
-    int a;
-    scanf("%d",&a);
-    // for(int i=1;i<=a;i++){
-    //     for(int j=0;j<=i-1;j++){
-    //         printf("%d ",j);
-    //     }
-    //     printf("\n");
-    // }
-    if(a==1){
-        printf("1");
-    }
-    if(a==2){
-        printf("1\n");
-        printf("0 1");
-    }
-    if(a==3){
-        printf("1\n");
-        printf("0 1\n");
-        printf("1 0 1");
-    }
-    if(a==4){
-        printf("1\n");
-        printf("0 1\n");
-        printf("1 0 1\n");
-        printf("0 1 0 1\n");
-    }
-    if(a==5){
-        printf("1\n");
-        printf("0 1\n");
-        printf("1 0 1\n");
-        printf("0 1 0 1\n");
-        printf("1 0 1 0 1\n");
-    }
-    if(a==6){
-        printf("1\n");
-        printf("0 1\n");
-        printf("1 0 1\n");
-        printf("0 1 0 1\n");
-        printf("1 0 1 0 1\n");
-        printf("0 1 0 1 0 1\n");
-    }
-    if(a==7){
-        printf("1\n");
-        printf("0 1\n");
-        printf("1 0 1\n");
-        printf("0 1 0 1\n");
-        printf("1 0 1 0 1\n");
-        printf("0 1 0 1 0 1\n");
-        printf("1 0 1 0 1 0 1\n");
-    }
-    
+    int N;
+    scanf("%d", &N);
 
+    for (int i = 1; i <= N; i++) {
+        int start = i % 2;  
+        for (int j = 0; j < i; j++) {
+            printf("%d ", start);
+            start = 1 - start;  
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
