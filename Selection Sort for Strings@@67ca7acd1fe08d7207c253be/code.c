258a30ae5e242bf4faac70186b0787579149bdf7
ayush <ayush0188.becse24@chitkara.edu.in>
2025-03-30 22:14:44 +0530

    Solution to Selection Sort for Strings

---
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -0,0 +1,19 @@
+int selectionSort(char arr[][],int n){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
+            if(arr[i][j]<arr[i-1][j-1]){
+                char temp[]=arr[i-1][j-1];
+                arr[i-1][j-1]=arr[i][j];
+                arr[i][j]=temp;
+
+                }
+        }
+    }
+    }
+int printArray(char arr[][],int n ){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%s ",arr[i][j]);
+        }
+    }
+}
