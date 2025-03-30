cbad925f82f4bf62a263206457d3ada4735a606f
ayush <ayush0188.becse24@chitkara.edu.in>
2025-03-30 22:02:31 +0530

    Solution to Selection Sort for Characters

---
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -0,0 +1,16 @@
+int selectionSort(char arr[],n){
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[j]){
+                char temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
+int printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ";arr[i]);
+    }
+}
