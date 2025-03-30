3cd14b908d521a0c077033414a043c851d661d3c
ayush <ayush0188.becse24@chitkara.edu.in>
2025-03-30 23:33:54 +0530

    Solution to String Compression

---
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -0,0 +1,15 @@
+int compressString(int str[],int comp[]){
+    int count=1;
+    int index=0;
+    for(int i=1;i<strlen(str);i++){
+          if(str[i]==str[i+1]){
+            count++;
+            else{
+                comp[index++]=str[i];
+                if(count>1){
+                    comp[index++]=count+'0';
+            }
+            count=1;
+          }  }
+          comp[index]='\0';
+}
