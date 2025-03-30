1f76be6bcde6c03c3cb9b4de39384cd0f366374d
ayush <ayush0188.becse24@chitkara.edu.in>
2025-03-30 22:37:46 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,6 +1,7 @@
 void caesarCipher(char mess[],int shift,char enc[]){
     for(int i=0;mess[i]!='\0';i++){
+        if(mess[i]!=NULL){
         enc[i]=mess[i]+shift;
-        enc[i+1]='\0';
+        enc[i+1]='\0';}
     }
 }
